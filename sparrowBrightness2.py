486fb9021aa9738a30a8b303da419705abaad97b
Sparrow <wroblewski.m@int.pl>
2024-09-28 23:20:31 +0200

    1.7

--- sparrowBrightness2.py
+++ sparrowBrightness2.py
@@ -1,88 +1,96 @@
-import sys
 import os
+import sys
+import tkinter as tk
+from tkinter import ttk, messagebox
+from tkinter import simpledialog
 import subprocess
-from PyQt5 import QtWidgets, QtGui, QtCore
 
-class BrightnessControl(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('Brightness Control')
-        self.setWindowIcon(QtGui.QIcon(self.resource_path('images/brightness-icon.png')))
-
-        self.brightness_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
-        self.brightness_slider.setMinimum(20)
-        self.brightness_slider.setMaximum(100)
-        self.brightness_slider.setValue(self.get_brightness())
-        self.brightness_slider.valueChanged.connect(self.set_brightness)
-
-        self.brightness_label = QtWidgets.QLabel(f'{self.brightness_slider.value()}%', self)
-
-        self.language_combo = QtWidgets.QComboBox(self)
-        self.language_combo.addItem('English')
-        self.language_combo.addItem('Polski')
-        self.language_combo.currentIndexChanged.connect(self.change_language)
-
-        self.tray_icon = QtWidgets.QSystemTrayIcon(self)
-        self.tray_icon.setIcon(QtGui.QIcon(self.resource_path('images/brightness-icon.png')))
-        self.tray_icon.activated.connect(self.tray_icon_activated)
-
-        self.exit_action = QtWidgets.QAction("Exit", self)
-        self.exit_action.triggered.connect(QtWidgets.qApp.quit)
-
-        self.tray_menu = QtWidgets.QMenu(self)
-        self.tray_menu.addAction(self.exit_action)
-        self.tray_icon.setContextMenu(self.tray_menu)
-
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.brightness_slider)
-        layout.addWidget(self.brightness_label)
-        layout.addWidget(self.language_combo)
-        self.setLayout(layout)
-
-        self.tray_icon.show()
-
-    def get_brightness(self):
-        result = subprocess.run(['xrandr', '--verbose'], stdout=subprocess.PIPE)
-        for line in result.stdout.decode().split('\n'):
-            if 'Brightness' in line:
-                return int(float(line.split()[1]) * 100)
-        return 100
-
-    def set_brightness(self, value):
-        subprocess.run(['xrandr', '--output', 'eDP-1', '--brightness', str(value / 100)])
-        self.brightness_label.setText(f'{value}%')
-
-    def change_language(self, index):
-        if index == 0:
-            self.setWindowTitle('Brightness Control')
-        else:
-            self.setWindowTitle('Podświetlenie ekranu')
-
-    def tray_icon_activated(self, reason):
-        if reason == QtWidgets.QSystemTrayIcon.Trigger:
-            self.show()
-
-    def closeEvent(self, event):
-        reply = QtWidgets.QMessageBox.question(self, 'Message',
-                                               "Do you want to minimize to tray instead of exiting?",
-                                               QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
-                                               QtWidgets.QMessageBox.Yes)
-        if reply == QtWidgets.QMessageBox.Yes:
-            event.ignore()
-            self.hide()
-        else:
-            event.accept()
+class SparrowBrightnessApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("SparrowBrightness")
+        self.root.iconphoto(False, tk.PhotoImage(file=self.resource_path("images/brightness-icon.png")))
+        self.language = self.load_language()
+        self.create_widgets()
+        self.update_ports()
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def resource_path(self, relative_path):
-        """ Get absolute path to resource, works for dev and for PyInstaller """
-        base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+        try:
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
         return os.path.join(base_path, relative_path)
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    ex = BrightnessControl()
-    ex.show()
-    sys.exit(app.exec_())
+    def load_language(self):
+        try:
+            with open("language.txt", "r") as file:
+                return file.read().strip()
+        except FileNotFoundError:
+            return "English"
+
+    def save_language(self, language):
+        with open("language.txt", "w") as file:
+            file.write(language)
+
+    def create_widgets(self):
+        self.language_var = tk.StringVar(value=self.language)
+        self.language_menu = ttk.Combobox(self.root, textvariable=self.language_var, values=["English", "Polski"])
+        self.language_menu.bind("<<ComboboxSelected>>", self.change_language)
+        self.language_menu.pack()
+
+        self.brightness_label = tk.Label(self.root, text=self.get_text("Brightness control"))
+        self.brightness_label.pack()
+
+        self.brightness_scale = tk.Scale(self.root, from_=10, to=100, orient="horizontal", command=self.update_brightness)
+        self.brightness_scale.pack()
+
+        self.brightness_value_label = tk.Label(self.root, text="{}%".format(self.brightness_scale.get()))
+        self.brightness_value_label.pack()
+
+        self.exit_option = tk.BooleanVar(value=True)
+        self.exit_checkbox = tk.Checkbutton(self.root, text=self.get_text("Exit on close"), variable=self.exit_option)
+        self.exit_checkbox.pack()
+
+    def get_text(self, text):
+        translations = {
+            "English": {
+                "Brightness control": "Brightness control",
+                "Exit on close": "Exit on close"
+            },
+            "Polski": {
+                "Brightness control": "Podświetlenie ekranu",
+                "Exit on close": "Zamknij przy zamknięciu"
+            }
+        }
+        return translations[self.language].get(text, text)
+
+    def change_language(self, event):
+        self.language = self.language_var.get()
+        self.save_language(self.language)
+        self.brightness_label.config(text=self.get_text("Brightness control"))
+        self.exit_checkbox.config(text=self.get_text("Exit on close"))
+
+    def update_brightness(self, value):
+        self.brightness_value_label.config(text="{}%".format(value))
+        # Here you would add the code to actually change the brightness using xrandr
+
+    def update_ports(self):
+        result = subprocess.run(["xrandr"], capture_output=True, text=True)
+        lines = result.stdout.split("\n")
+        for line in lines:
+            if " connected" in line or " disconnected" in line:
+                port_name = line.split()[0]
+                label = tk.Label(self.root, text=port_name, fg="grey" if "disconnected" in line else "black")
+                label.pack()
+
+    def on_closing(self):
+        if self.exit_option.get():
+            self.root.destroy()
+        else:
+            self.root.iconify()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SparrowBrightnessApp(root)
+    root.mainloop()
